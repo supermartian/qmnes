079b2050dbae1b9e02fbd5860f7db88fc12f9530
Yuzhong Wen <supermartian@gmail.com>
2014-06-24 02:58:17 -0400

    Add test for CPU

---
+++ test.c
@@ -0,0 +1,37 @@
+/*
+ * test.c
+ * Copyright (C) 2014 martian <supermartian@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+
+#include "cpu.h"
+#include "rom.h"
+#include "ins.h"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        printf("oops\n");
+        return 1;
+    }
+
+    char *filename = argv[1];
+
+    struct rom test_rom;
+    struct cpu_6502 cpu;
+
+    cpu_setup(&cpu);
+
+    load_rom(&test_rom, filename);
+    printf("Prog size %x\n", test_rom.prg_rom_data);
+    cpu.rom_prg = test_rom.prg_rom_data;
+    printf("Prog size %x\n", cpu.rom_prg);
+    
+    cpu_reset(&cpu);
+    cpu_run(&cpu);
+
+    return 0;
+}
